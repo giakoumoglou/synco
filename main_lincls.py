5dd24efacedbd249d9907add5aed86c2008f36b8
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-08-22 11:37:37 +0300

    Update main_lincls.py

--- main_lincls.py
+++ main_lincls.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-
-# This source code is licensed under the MIT license found in the
+# Copyright (C) 2024. All rights reserved.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
 
