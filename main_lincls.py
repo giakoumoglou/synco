12253d461b5ad6856fde18deebf2bcf7d05ac4a2
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-08-24 16:37:46 +0300

    Update main_lincls.py

--- main_lincls.py
+++ main_lincls.py
@@ -39,7 +39,7 @@ parser.add_argument("--weights", default="freeze", type=str, choices=("finetune"
 parser.add_argument("-a", "--arch", metavar="ARCH", default="resnet50", choices=model_names, help="model architecture: " + " | ".join(model_names) + " (default: resnet50)",)
 parser.add_argument("-j", "--workers", default=32, type=int, metavar="N", help="number of data loading workers (default: 32)",)
 parser.add_argument("--epochs", default=100, type=int, metavar="N", help="number of total epochs to run")
-parser.add_argument( "--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)",)
+parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)",)
 parser.add_argument("-b", "--batch-size", default=256, type=int, metavar="N", help="mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel",)
 parser.add_argument("--lr", "--learning-rate",  default=0.03,  type=float, metavar="LR",  help="initial learning rate",  dest="lr",)
 parser.add_argument("--schedule", default=[60, 80], nargs="*", type=int, help="learning rate schedule (when to drop lr by a ratio)",)
