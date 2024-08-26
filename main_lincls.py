426b3a2098d261cf581a62a8bc4657241cac391d
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-08-26 11:17:54 +0300

    Update main_lincls.py

--- main_lincls.py
+++ main_lincls.py
@@ -34,27 +34,27 @@ model_names = sorted(name for name in models.__dict__ if name.islower() and not 
 
 parser = argparse.ArgumentParser(description="PyTorch ImageNet Training")
 parser.add_argument("data", metavar="DIR", help="path to dataset")
-parser.add_argument("-a", "--arch", metavar="ARCH", default="resnet50", choices=model_names, help="model architecture: " + " | ".join(model_names) + " (default: resnet50)",)
-parser.add_argument("-j", "--workers", default=32, type=int, metavar="N", help="number of data loading workers (default: 32)",)
+parser.add_argument("-a", "--arch", metavar="ARCH", default="resnet50", choices=model_names, help="model architecture: " + " | ".join(model_names) + " (default: resnet50)")
+parser.add_argument("-j", "--workers", default=32, type=int, metavar="N", help="number of data loading workers (default: 32)")
 parser.add_argument("--epochs", default=100, type=int, metavar="N", help="number of total epochs to run")
-parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)",)
-parser.add_argument("-b", "--batch-size", default=256, type=int, metavar="N", help="mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel",)
-parser.add_argument("--lr", "--learning-rate",  default=0.03,  type=float, metavar="LR",  help="initial learning rate",  dest="lr",)
-parser.add_argument("--schedule", default=[60, 80], nargs="*", type=int, help="learning rate schedule (when to drop lr by a ratio)",)
+parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)")
+parser.add_argument("-b", "--batch-size", default=256, type=int, metavar="N", help="mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel")
+parser.add_argument("--lr", "--learning-rate",  default=0.03,  type=float, metavar="LR",  help="initial learning rate",  dest="lr")
+parser.add_argument("--schedule", default=[60, 80], nargs="*", type=int, help="learning rate schedule (when to drop lr by a ratio)")
 parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
-parser.add_argument("--wd", "--weight-decay", default=0.0, type=float, metavar="W", help="weight decay (default: 0.)",  dest="weight_decay",)
-parser.add_argument("-p", "--print-freq", default=10, type=int, metavar="N", help="print frequency (default: 10)",)
-parser.add_argument("--resume", default="",  type=str, metavar="PATH", help="path to latest checkpoint (default: none)",)
-parser.add_argument("-e", "--evaluate", dest="evaluate", action="store_true", help="evaluate model on validation set",)
-parser.add_argument("--world-size", default=-1, type=int, help="number of nodes for distributed training",)
+parser.add_argument("--wd", "--weight-decay", default=0.0, type=float, metavar="W", help="weight decay (default: 0.)",  dest="weight_decay")
+parser.add_argument("-p", "--print-freq", default=10, type=int, metavar="N", help="print frequency (default: 10)")
+parser.add_argument("--resume", default="",  type=str, metavar="PATH", help="path to latest checkpoint (default: none)")
+parser.add_argument("-e", "--evaluate", dest="evaluate", action="store_true", help="evaluate model on validation set")
+parser.add_argument("--world-size", default=-1, type=int, help="number of nodes for distributed training")
 parser.add_argument("--rank", default=-1, type=int, help="node rank for distributed training")
-parser.add_argument("--dist-url", default="tcp://224.66.41.62:23456", type=str, help="url used to set up distributed training",)
+parser.add_argument("--dist-url", default="tcp://224.66.41.62:23456", type=str, help="url used to set up distributed training")
 parser.add_argument("--dist-backend", default="nccl", type=str, help="distributed backend")
-parser.add_argument("--seed", default=None, type=int, help="seed for initializing training. ")
+parser.add_argument("--seed", default=None, type=int, help="seed for initializing training.")
 parser.add_argument("--gpu", default=None, type=int, help="GPU id to use.")
-parser.add_argument("--multiprocessing-distributed", action="store_true", help="Use multi-processing distributed training to launch N processes per node, which has N GPUs. This is the fastest way to use PyTorch for either single node or multi node data parallel training",)
+parser.add_argument("--multiprocessing-distributed", action="store_true", help="use multi-processing distributed training to launch N processes per node, which has N GPUs. This is the fastest way to use PyTorch for either single node or multi node data parallel training")
 
-parser.add_argument("--pretrained", default="", type=str, help="path to moco pretrained checkpoint")
+parser.add_argument("--pretrained", default="", type=str, help="path to pretrained checkpoint")
 
 best_acc1 = 0
 
@@ -127,6 +127,7 @@ def main_worker(gpu, ngpus_per_node, args):
             world_size=args.world_size,
             rank=args.rank,
         )
+        
     # create model
     print("=> creating model '{}'".format(args.arch))
     model = models.__dict__[args.arch]()
@@ -145,9 +146,7 @@ def main_worker(gpu, ngpus_per_node, args):
             state_dict = checkpoint["state_dict"]
             for k in list(state_dict.keys()):
                 # retain only encoder_q up to before the embedding layer
-                if k.startswith("module.encoder_q") and not k.startswith(
-                    "module.encoder_q.fc"
-                ):
+                if k.startswith("module.encoder_q") and not k.startswith("module.encoder_q.fc"):
                     # remove prefix
                     state_dict[k[len("module.encoder_q.") :]] = state_dict[k]
                 # delete renamed or unused k
@@ -225,7 +224,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     cudnn.benchmark = True
 
-    # Data loading code
+    # data loading code
     traindir = os.path.join(args.data, "train")
     valdir = os.path.join(args.data, "val")
     normalize = transforms.Normalize(
