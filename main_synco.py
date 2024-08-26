8f99107cf465647a48e257f2df686ed1c09f5b34
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-08-26 11:21:22 +0300

    Update main_synco.py

--- main_synco.py
+++ main_synco.py
@@ -36,25 +36,25 @@ model_names = sorted(name for name in models.__dict__ if name.islower() and not 
 
 parser = argparse.ArgumentParser(description="PyTorch ImageNet Training")
 parser.add_argument("data", metavar="DIR", help="path to dataset")
-parser.add_argument("-a", "--arch", metavar="ARCH", default="resnet50", choices=model_names, help="model architecture: " + " | ".join(model_names) + " (default: resnet50)",)
-parser.add_argument("-j", "--workers", default=32, type=int, metavar="N", help="number of data loading workers (default: 32)",)
+parser.add_argument("-a", "--arch", metavar="ARCH", default="resnet50", choices=model_names, help="model architecture: " + " | ".join(model_names) + " (default: resnet50)")
+parser.add_argument("-j", "--workers", default=32, type=int, metavar="N", help="number of data loading workers (default: 32)")
 parser.add_argument("--epochs", default=200, type=int, metavar="N", help="number of total epochs to run")
-parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)",)
-parser.add_argument("-b", "--batch-size", default=256, type=int, metavar="N", help="mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel",)
-parser.add_argument("--lr", "--learning-rate",  default=0.03,  type=float, metavar="LR",  help="initial learning rate",  dest="lr",)
-parser.add_argument("--schedule", default=[120, 160], nargs="*", type=int, help="learning rate schedule (when to drop lr by 10x)",)
+parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="manual epoch number (useful on restarts)")
+parser.add_argument("-b", "--batch-size", default=256, type=int, metavar="N", help="mini-batch size (default: 256), this is the total batch size of all GPUs on the current node when using Data Parallel or Distributed Data Parallel")
+parser.add_argument("--lr", "--learning-rate",  default=0.03,  type=float, metavar="LR",  help="initial learning rate",  dest="lr")
+parser.add_argument("--schedule", default=[120, 160], nargs="*", type=int, help="learning rate schedule (when to drop lr by 10x)")
 parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum of SGD solver")
-parser.add_argument("--wd", "--weight-decay", default=1e-4, type=float, metavar="W", help="weight decay (default: 1e-4)", dest="weight_decay",)
-parser.add_argument("-p", "--print-freq", default=10, type=int, metavar="N", help="print frequency (default: 10)",)
-parser.add_argument("--resume", default="", type=str, metavar="PATH", help="path to latest checkpoint (default: none)",)
-parser.add_argument("--world-size", default=-1, type=int, help="number of nodes for distributed training",)
+parser.add_argument("--wd", "--weight-decay", default=1e-4, type=float, metavar="W", help="weight decay (default: 1e-4)", dest="weight_decay")
+parser.add_argument("-p", "--print-freq", default=10, type=int, metavar="N", help="print frequency (default: 10)")
+parser.add_argument("--resume", default="", type=str, metavar="PATH", help="path to latest checkpoint (default: none)")
+parser.add_argument("--world-size", default=-1, type=int, help="number of nodes for distributed training")
 parser.add_argument("--rank", default=-1, type=int, help="node rank for distributed training")
-parser.add_argument("--dist-url", default="tcp://224.66.41.62:23456", type=str, help="url used to set up distributed training",)
+parser.add_argument("--dist-url", default="tcp://224.66.41.62:23456", type=str, help="url used to set up distributed training")
 parser.add_argument("--dist-backend", default="nccl", type=str, help="distributed backend")
-parser.add_argument("--seed", default=None, type=int, help="seed for initializing training. ")
-parser.add_argument("--gpu", default=None, type=int, help="GPU id to use.")
-parser.add_argument("--multiprocessing-distributed", action="store_true", help="Use multi-processing distributed training to launch N processes per node, which has N GPUs. This is the fastest way to use PyTorch for either single node or multi node data parallel training",)
-parser.add_argument("--save-dir", default="./results", type=str, help="directory to save checkpoints (default: './results')")
+parser.add_argument("--seed", default=None, type=int, help="seed for initializing training")
+parser.add_argument("--gpu", default=None, type=int, help="GPU id to use")
+parser.add_argument("--multiprocessing-distributed", action="store_true", help="use multi-processing distributed training to launch N processes per node, which has N GPUs. This is the fastest way to use PyTorch for either single node or multi node data parallel training")
+parser.add_argument("--save-dir", default="./output/", type=str, help="directory to save checkpoints (default: './output/')")
 
 # moco specific configs:
 parser.add_argument("--moco-dim", default=128, type=int, help="feature dimension (default: 128)")
@@ -75,10 +75,10 @@ parser.add_argument("--n3", default=256, type=int, help="number of hard negative
 parser.add_argument("--n4", default=64, type=int, help="number of hard negatives to generate by noise injection (default: 64)")
 parser.add_argument("--n5", default=64, type=int, help="number of hard negatives to generate by gradient-based method (default: 64)")
 parser.add_argument("--n6", default=64, type=int, help="number of hard negatives to generate by adversarial method (default: 64)")
-parser.add_argument("--sigma", default=0.1, type=float, help="sigma parameter for gradient-based hard negatives") 
-parser.add_argument("--delta", default=0.01, type=float, help="delta parameter for noise injection hard negatives") 
-parser.add_argument("--eta", default=0.01, type=float, help="epsilon parameter for adversarial hard negatives") 
-parser.add_argument("--warmup-epochs", default=10, type=int, help="number of warmup epochs where no synthetic negatives are generated")
+parser.add_argument("--sigma", default=0.01, type=float, help="sigma parameter for gradient-based hard negatives (default: 0.01)") 
+parser.add_argument("--delta", default=0.01, type=float, help="delta parameter for noise injection hard negatives (default: 0.01)") 
+parser.add_argument("--eta", default=0.01, type=float, help="epsilon parameter for adversarial hard negatives (default: 0.01)") 
+parser.add_argument("--warmup-epochs", default=10, type=int, help="number of warmup epochs where no synthetic negatives are generated (default: 10)")
 
 
 def main():
@@ -147,6 +147,7 @@ def main_worker(gpu, ngpus_per_node, args):
             world_size=args.world_size,
             rank=args.rank,
         )
+        
     # create model
     print("=> creating model '{}'".format(args.arch))
     model = synco.builder.SynCo(
@@ -217,23 +218,19 @@ def main_worker(gpu, ngpus_per_node, args):
             if args.gpu is None:
                 checkpoint = torch.load(args.resume)
             else:
-                # Map model to be loaded to specified single gpu.
+                # map model to be loaded to specified single gpu.
                 loc = "cuda:{}".format(args.gpu)
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint["epoch"]
             model.load_state_dict(checkpoint["state_dict"])
             optimizer.load_state_dict(checkpoint["optimizer"])
-            print(
-                "=> loaded checkpoint '{}' (epoch {})".format(
-                    args.resume, checkpoint["epoch"]
-                )
-            )
+            print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint["epoch"]))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
     cudnn.benchmark = True
 
-    # Data loading code
+    # data loading code
     traindir = os.path.join(args.data, "train")
     normalize = transforms.Normalize( mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     
