0c8fd9fc86eb27a5d75f850888b791be3c757b15
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-08-28 17:45:22 +0300

    Update download_voc.sh

--- detection/datasets/download_voc.sh
+++ detection/datasets/download_voc.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-cd $PBS_O_WORKDIR
-
 mkdir -p VOC2007 VOC2012
 
-# Download VOC 2007 trainval
 wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar -P VOC2007
-
-# Download VOC 2007 test
 wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar -P VOC2007
-
-# Download VOC 2012 trainval
 wget -c http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar -P VOC2012
 
-# Extracting archives
 tar -xf VOC2007/VOCtrainval_06-Nov-2007.tar -C VOC2007
 tar -xf VOC2007/VOCtest_06-Nov-2007.tar -C VOC2007
 tar -xf VOC2012/VOCtrainval_11-May-2012.tar -C VOC2012
