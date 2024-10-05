89e6f3d403d223d10ca4e4e4d9b524df5549e2e4
Nikos Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-10-05 14:35:49 +0300

    Add files via upload

---
+++ detection/scripts/synco_voc_200ep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -lselect=1:ncpus=32:mem=96gb:ngpus=4
+#PBS -lwalltime=72:00:00
+
+cd $PBS_O_WORKDIR
+
+module load anaconda3/personal
+source activate detectron2_cuda10
+
+echo "Checking NVIDIA GPU status with nvidia-smi:"
+nvidia-smi
+echo "Checking CUDA compiler version with nvcc:"
+nvcc --version
+
+python train_net.py --config-file configs/pascal_voc_R_50_C4_24k_moco.yaml --num-gpus 4 MODEL.WEIGHTS ./resnet50_synco_200ep.pkl
