7441c521859a2c76128a2ba57e3d2516a84aa7b0
Nikos Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2025-02-25 15:31:14 +0200

    Update synco_r50_800ep.sh

--- scripts/synco_r50_800ep.sh
+++ scripts/synco_r50_800ep.sh
@@ -7,4 +7,4 @@ cd $PBS_O_WORKDIR
 eval "$(~/anaconda3/bin/conda shell.bash hook)"
 source activate torch
 
-python main_synco.py -a resnet50 --lr 0.03 --batch-size 256 --epochs 800 --resume $EPHEMERAL/synco/synco_r50_800ep/checkpoint_0660.pth.tar --save-dir $EPHEMERAL/synco/synco_r50_800ep/ --dist-url 'tcp://localhost:10302' --multiprocessing-distributed --world-size 1 --rank 0 --mlp --moco-t 0.2 --aug-plus --cos --n-hard 1024 --n1 256 --n2 256 --n3 256 --n4 64 --n5 64 --n6 64 $HOME/datasets/imagenet/
+python main_synco.py -a resnet50 --lr 0.03 --batch-size 256 --epochs 800 --resume ./checkpoints/synco_r50_800ep/checkpoint_0660.pth.tar --save-dir ./checkpoints/synco/synco_r50_800ep/ --dist-url 'tcp://localhost:10302' --multiprocessing-distributed --world-size 1 --rank 0 --mlp --moco-t 0.2 --aug-plus --cos --n-hard 1024 --n1 256 --n2 256 --n3 256 --n4 64 --n5 64 --n6 64 ./datasets/imagenet/
