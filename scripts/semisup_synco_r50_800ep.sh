0d0faef6b18d9b348dfd5d025f92c4cf56474554
Nikos Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2025-02-25 15:30:18 +0200

    Update semisup_synco_r50_800ep.sh

--- scripts/semisup_synco_r50_800ep.sh
+++ scripts/semisup_synco_r50_800ep.sh
@@ -11,8 +11,8 @@ learning_rates=(0.01 0.02 0.05 0.1 0.005)
 
 for lr in "${learning_rates[@]}"; do
     echo "========== LR: $lr, Percentage 1% ==========="
-    python main_semisup.py -a resnet50 --lr-backbone $lr --lr-classifier 0.5 --epochs 60 --train-percent 1 --weights finetune --batch-size 1024 --pretrained $EPHEMERAL/synco/synco_r50_800ep/checkpoint_0799.pth.tar --dist-url 'tcp://localhost:17002' --multiprocessing-distributed --world-size 1 --rank 0 $HOME/datasets/imagenet/
+    python main_semisup.py -a resnet50 --lr-backbone $lr --lr-classifier 0.5 --epochs 60 --train-percent 1 --weights finetune --batch-size 1024 --pretrained ./checkpoints/synco_r50_800ep/checkpoint_0799.pth.tar --dist-url 'tcp://localhost:17002' --multiprocessing-distributed --world-size 1 --rank 0 ./datasets/imagenet/
     
     echo "========== LR: $lr, Percentage 10% ==========="
-    python main_semisup.py -a resnet50 --lr-backbone $lr --lr-classifier 0.5 --epochs 30 --train-percent 10 --weights finetune --batch-size 1024 --pretrained $EPHEMERAL/synco/synco_r50_800ep/checkpoint_0799.pth.tar --dist-url 'tcp://localhost:17002' --multiprocessing-distributed --world-size 1 --rank 0 $HOME/datasets/imagenet/
+    python main_semisup.py -a resnet50 --lr-backbone $lr --lr-classifier 0.5 --epochs 30 --train-percent 10 --weights finetune --batch-size 1024 --pretrained ./checkpoints/synco_r50_800ep/checkpoint_0799.pth.tar --dist-url 'tcp://localhost:17002' --multiprocessing-distributed --world-size 1 --rank 0 ./datasets/imagenet/
 done
